a488859a08e0bf7baf9a454859d4b9aec3445cd8
BinayaShrestha556 <binayastha37b@gmail.com>
2023-07-20 16:39:08 +0545

    added returnuing object example (add 2 complex number)

---
+++ return_obj_add_2complx.cpp
@@ -0,0 +1,30 @@
+//add 2 complex number
+#include <iostream>
+using namespace std;
+class Complex{
+    private:
+    int real,img;
+    public:
+    void getData(){
+        cout<<"enter complex number, real and imaginary numbers: ";
+        cin>>real>>img;
+
+    }
+    Complex add(Complex c){
+        Complex temp;
+        temp.real=real+c.real;
+        temp.img=img+c.img;
+        return temp;
+    }
+    void print(){
+        cout<<"the answer is: "<<real<<"+"<<img<<"i";
+
+    }
+};
+int main(){
+    Complex c1,c2,c3;
+    c1.getData();
+    c2.getData();
+    c3=c1.add(c2);
+    c3.print();
+}
